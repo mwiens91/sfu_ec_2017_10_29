9dede858e425bacd2bd53719c996f54318fd136d
Avery Jones <averyj@sfu.ca>
2017-10-29 11:22:59 -0700

    working on buffers

--- safetrain/train.py
+++ safetrain/train.py
@@ -11,10 +11,17 @@ class TrainState(Enum):
 
 class Train:
     """Docstring here."""
-    def __init__(self, idnum=00000, state=TrainState.NONE):
+    def __init__(self, idnum=00000, state=TrainState.NONE, velocity):
         self.idnum = idnum
         self.state = state
-
+        self.velocity = velocity
+        self.frontRed = 0
+        self.frontYellow = 0
+        self.backBlue = 0
+    def calculateBuffer(self):
+        if self.velocity >= 51:
+            self.frontRed = 121.4 + (((velocity-51)/37)*51.8)
+            self.frontYellow = 217.3 + (velocity-)
 
 if __name__ == "__main__":
     # Self-test code
