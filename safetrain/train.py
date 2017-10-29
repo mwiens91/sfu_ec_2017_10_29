9776faf13f81e487ce311a57ccd135c1a085baa9
Avery Jones <averyj@sfu.ca>
2017-10-29 11:40:12 -0700

    buffers done

--- safetrain/train.py
+++ safetrain/train.py
@@ -20,9 +20,21 @@ class Train:
         self.backBlue = 0
     def calculateBuffer(self):
         if self.velocity >= 51:
-            self.frontRed = 121.4 + (((velocity-51)/37)*51.8)
-            self.frontYellow = 217.3 + (velocity-)
-
+            self.frontRed = 121.4 + (((self.velocity-51)/37)*51.8)
+            self.frontYellow = 217.3 + (((self.velocity-51)/37)*85.1)
+            self.backBlue = 82.15 - (((self.velocity - 51)/37)*12.95)
+        elif self.velocity >= 31:
+            self.frontRed = 88.75 + (((self.velocity-31)/19)*23.75)
+            self.frontYellow = 162 + (((self.velocity-31)/19)*38)
+            self.backBlue = 89.15 - (((self.velocity - 31)/19)*(-6.65))
+        elif self.velocity >= 1:
+            self.frontRed = 51.1 + (((self.velocity-1)/30)*31.9)
+            self.frontYellow = 101.8 + (((self.velocity-1)/30)*52.2)
+            self.backBlue = 99.65 - (((self.velocity - 1)/30)*(-10.15))
+        else:
+            self.frontRed = 50
+            self.frontYellow = 100
+            self.backBlue = 100
 if __name__ == "__main__":
     # Self-test code
     train1 = Train()
