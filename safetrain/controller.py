28cd73ed19d4fdf503452f7464e044f920799ae7
Calvin Liu <calvinliu25@yahoo.ca>
2017-10-29 11:57:32 -0700

    Update controller.py

--- safetrain/controller.py
+++ safetrain/controller.py
@@ -9,12 +9,12 @@ class Controller:
         attr1: herp derp
     """
     def __init__(self):
-        Train(00001, IDLING, 0)
-        Train(00002, IDLING, 0)
-        Train(00003, IDLING, 0)
-        Train(00004, IDLING, 0)
-        Train(00005, IDLING, 0)
-        Train(00006, IDLING, 0)
+        Train('00001', IDLING, 0)
+        Train('00002', IDLING, 0)
+        Train('00003', IDLING, 0)
+        Train('00004', IDLING, 0)
+        Train('00005', IDLING, 0)
+        Train('00006', IDLING, 0)
 
 
 if __name__ == "__main__":
