12ece67699660645bd3b1e995783ab97e5220082
Calvin Liu <calvinliu25@yahoo.ca>
2017-10-29 11:34:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 """Safetrain v9.0"""
 
+from safetrain import logging
+from safetrain import controller
+from safetrain import train
+
 def main():
     """Main function for Safetrain."""
-    print("HI")
+    initialize_log()
+    logger = logging.getLogger('safetrain')
 
 if __name__ == '__main__':
     main()
