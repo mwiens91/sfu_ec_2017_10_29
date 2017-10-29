a37ecf6d2121e1c1a743931332f3dc53b402833a
Matt Wiens <mwiens91@gmail.com>
2017-10-29 10:48:07 -0700

    Start init for trains

--- safetrain/train.py
+++ safetrain/train.py
@@ -1,1 +1,21 @@
 """Train class and related functions."""
+
+from enum import Enum
+
+class TrainState(Enum):
+    """Docstring here."""
+    INMOTION = 1
+    IDLING = 2
+    OOS = 3
+    NONE = 4
+
+class Train:
+    """Docstring here."""
+    def __init__(self, idnum=00000, state=TrainState.NONE):
+        self.idnum = idnum
+        self.state = state
+
+
+if __name__ == "__main__":
+    # Self-test code
+    train1 = Train()
