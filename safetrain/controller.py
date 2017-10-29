80fa6d0ea89e34f497d6fec541f188c54f0286e0
Matt Wiens <mwiens91@gmail.com>
2017-10-29 11:16:58 -0700

    Add controller file

---
+++ safetrain/controller.py
@@ -0,0 +1,12 @@
+"""Controller class and related functions."""
+
+from train import Train
+
+class Controller:
+    """Docstring here."""
+    def __init__(self):
+        # Useful things here
+
+if __name__ == "__main__":
+    # Self-test code
+    Controller()
