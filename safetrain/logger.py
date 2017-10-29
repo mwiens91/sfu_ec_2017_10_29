504b8e6f52a3451540c3860bb9781b89816e2253
Matt Wiens <mwiens91@gmail.com>
2017-10-29 11:45:20 -0700

    Add more docstrings

--- safetrain/logger.py
+++ safetrain/logger.py
@@ -1,17 +1,19 @@
-"""Logging"""
+"""Contains a logger to print results to console/file."""
 
 import time
 import logging
 
 def initialize_log():
-	logging.basicConfig(
-		filename='result.txt',
-		filemode='a',
-		format='%(asctime)s,%(msecs)d - %(message)s',
-        	datefmt='%H:%M:%S',
-        	level=logging.DEBUG)
-	logging.info('##### Log Initialized #####')
+    """Initializes the logger."""
+    logging.basicConfig(
+        filename='result.txt',
+        filemode='a',
+        format='%(asctime)s,%(msecs)d - %(message)s',
+        datefmt='%H:%M:%S',
+        level=logging.DEBUG)
+    logging.info('##### Log Initialized #####')
 
 def log(message):
-	logging.info(message)
-	print(message)
+    """Logs a message to the logger."""
+    logging.info(message)
+    print(message)
