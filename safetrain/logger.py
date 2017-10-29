3ad12bc54e82810369e99839d0f6d5afcd2b33a0
Calvin Liu <calvinliu25@yahoo.ca>
2017-10-29 11:29:14 -0700

    Create logger.py

---
+++ safetrain/logger.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import time
+import logging
+
+def initialize_log():
+	logging.basicConfig(filename='result.txt',
+		filemode='a',
+			format='%(asctime)s,%(msecs)d - %(message)s',
+                	datefmt='%H:%M:%S',
+                	level=logging.DEBUG)
+	logging.info('##### Log Initialized #####')
+
+def log(message):
+	logging.info(message)
+	print(message)
